bcaaa246191a6554fff9cf74ece6a8766dd33110
EBAralph0 <edracresurek@gmail.com>
2020-11-19 11:08:32 +0100

    1

---
+++ CC01.BO/Ecole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ecole
+{
+	public string Name { get; set; }
+	public string PostalCode { get; set; }
+	public string PhoneNumber { get; set; }
+	public string email { get; set; }
+	public byte Logo { get; set; }
+	public Ecole()
+	{
+
+	}
+    public Ecole(string name, string postalCode, string phoneNumber, string email, byte logo)
+    {
+        Name = name;
+        PostalCode = postalCode;
+        PhoneNumber = phoneNumber;
+        Logo = logo;
+        Email = email;
+    }
+    public override bool Equals(object obj)
+    {
+        return obj is Ecole ecole &&
+            Name == ecole.Name;
+    }
+
+    public override int GetHashCode()
+    {
+        return -1304721846 + IEqualityComparer<string>.Default.GetHashcode(Name);
+    }
+}
---
+++ CC01.BO/Etudiant.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BO
+{
+    [Serializable]
+    public class Etudiant
+    {
+        public string Identified { get; set; }
+        public string Last_name { get; set; }
+        public string First_name { get; set; }
+        public DateTime Born_on { get; set; }
+        public long Contact { get; set; }
+        public string Email {get;set;}
+
+        public Etudiant()// constucteur vide important pour la sérialisation
+        {
+
+        }
+        public Etudiant(string identified,string last_name,string first_name, DateTime born_on,long contact,string email)
+        {
+            Identified=identified;
+            Last_name=last_name;
+            First_name=first_name;
+            Born_on=born_on;
+            Contact=contact;
+            Email=email;
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiant etudiant &&
+                Identified==etudiant.Identified;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1304721846+IEqualityComparer<string>.Default.GetHashcode(Identified);
+        }
+    }
+}
